c66e05f84b684144d120445d44a6559969521124
Fengming Sun <s@sfmblog.cn>
2015-11-03 00:44:04 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>Hello {this.props.name}!</div>
+    );
+  }
+});
+
+module.exports = App;
